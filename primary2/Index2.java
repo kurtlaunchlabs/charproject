8a5c9c5eaebd682fa0afb4e55eb6e9c97f12668d
Kimson Wong <kimsonwong@gmail.com>
2016-06-30 17:22:07 +0800

    Back to original

--- primary2/Index2.java
+++ primary2/Index2.java
@@ -2,7 +2,7 @@ public class Index2{
 
 
 	public void main(String args[]){
-		System.out.println("Hello is that me you looking for 2");
+		
 		System.out.println("This is new branch for Charproject 2");
 		
 	}
