75ee5039a1cfe0bf860f6379fdb57ad0135fed13
Kimson Wong <kimsonwong@gmail.com>
2016-06-30 16:29:23 +0800

    Saving all data file

---
+++ Index.java
@@ -0,0 +1,8 @@
+public class Index{
+
+
+	public void main(String args[]){
+		System.out.println("Hello is that me you looking for");
+		
+	}
+}
---
+++ primary2/Index2.java
@@ -0,0 +1,8 @@
+public class Index2{
+
+
+	public void main(String args[]){
+		System.out.println("Hello is that me you looking for 2");
+		
+	}
+}
