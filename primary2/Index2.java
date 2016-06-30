5776cd04bc7ba0cc0bee2a68735a085c2f8e13ef
Kimson Wong <kimsonwong@gmail.com>
2016-06-30 17:51:27 +0800

    Additional Modification

--- primary2/Index2.java
+++ primary2/Index2.java
@@ -4,6 +4,7 @@ public class Index2{
 	public void main(String args[]){
 		
 		System.out.println("This is new branch for Charproject 2");
+		System.out.println("This is new branch for Charproject modify2");
 		
 	}
 }
