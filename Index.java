98412d8f6167471b3630509f8cf79e2bf6e579ec
Kimson Wong <kimsonwong@gmail.com>
2016-06-30 16:47:04 +0800

    Added Branch for Project

--- Index.java
+++ Index.java
@@ -3,6 +3,7 @@ public class Index{
 
 	public void main(String args[]){
 		System.out.println("Hello is that me you looking for");
-		
+		System.out.println("This is new branch for Charproject 2");
+
 	}
 }
