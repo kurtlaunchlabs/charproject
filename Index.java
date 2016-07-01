65de1f2108211a3036b520bb14443daeabe3c2a5
Kimson Wong <kimsonwong@gmail.com>
2016-07-01 11:18:36 +0800

    This is an update for july1

--- Index.java
+++ Index.java
@@ -2,7 +2,11 @@ public class Index{
 
 
 	public void main(String args[]){
+
+		/* This is july1 update */
+
 		System.out.println("Hello this is a master branch");
+
 
 	}
 }
--- primary2/Index2.java
+++ primary2/Index2.java
@@ -3,6 +3,8 @@ public class Index2{
 
 	public void main(String args[]){
 		
+		/*This is a update of july1*/
+
 		System.out.println("This is a master branch and Index2");
 		
 	}
