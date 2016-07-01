ca65d7239fa26033283b24f330a5c815bb837a8d
kurtlaunchlabs <kurt_auxtero@launchlabs.ph>
2016-07-01 10:11:33 +0800

    Update Index.java

--- Index.java
+++ Index.java
@@ -3,7 +3,7 @@ public class Index{
 
 	public void main(String args[]){
 		System.out.println("Hello is that me you looking for");
-		System.out.println("Hello is that me you looking for 6");
+		System.out.println("Hello is that me you looking for 2");
 
 	}
 }
