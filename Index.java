c05c821934ad3b60b773eaf712d0452d307d6cf6
Kimson Wong <kimsonwong@gmail.com>
2016-07-01 11:03:32 +0800

    This is a master not default not a branch

--- Index.java
+++ Index.java
@@ -2,9 +2,7 @@ public class Index{
 
 
 	public void main(String args[]){
-		System.out.println("Hello is that me you looking for");
-		System.out.println("Hello is that me you looking for 2");
-		System.out.println("Hello World");
+		System.out.println("Hello this is a master branch");
 
 	}
 }
--- primary2/Index2.java
+++ primary2/Index2.java
@@ -3,8 +3,7 @@ public class Index2{
 
 	public void main(String args[]){
 		
-		System.out.println("This is new branch for Charproject 2");
-		System.out.println("This is new branch for Charproject modify22");
+		System.out.println("This is a master branch and Index2");
 		
 	}
 }
